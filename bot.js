93fd9443aa2cf741197073a22322624c7f9c1ea4
cdrbbt <egor.bykadorov@gmail.com>
2018-11-19 12:52:29 +0200

    cleaned up some glitch code

--- bot.js
+++ bot.js
@@ -52,9 +52,9 @@ This bot demonstrates many of the core features of Botkit:
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-if (!process.env.clientId || !process.env.clientSecret || !process.env.PORT) {
+if (!process.env.clientId || !process.env.clientSecret) {
   usage_tip();
-  // process.exit(1);
+  process.exit(1);
 }
 
 var Botkit = require('botkit');
@@ -86,26 +86,6 @@ controller.startTicking();
 
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
 var webserver = require(__dirname + '/components/express_webserver.js')(controller);
-
-if (!process.env.clientId || !process.env.clientSecret) {
-
-  // Load in some helpers that make running Botkit on Glitch.com better
-  require(__dirname + '/components/plugin_glitch.js')(controller);
-
-  webserver.get('/', function(req, res){
-    res.render('installation', {
-      studio_enabled: controller.config.studio_token ? true : false,
-      domain: req.get('host'),
-      protocol: req.protocol,
-      glitch_domain:  process.env.PROJECT_DOMAIN,
-      layout: 'layouts/default'
-    });
-  })
-
-  var where_its_at = 'https://' + process.env.PROJECT_DOMAIN + '.glitch.me/';
-  console.log('WARNING: This application is not fully configured to work with Slack. Please see instructions at ' + where_its_at);
-}else {
-
   webserver.get('/', function(req, res){
     res.render('index', {
       domain: req.get('host'),
@@ -114,6 +94,7 @@ if (!process.env.clientId || !process.env.clientSecret) {
       layout: 'layouts/default'
     });
   })
+
   // Set up a simple storage backend for keeping a record of customers
   // who sign up for the app via the oauth
   require(__dirname + '/components/user_registration.js')(controller);
@@ -124,7 +105,6 @@ if (!process.env.clientId || !process.env.clientSecret) {
   // Load in some helpers that make running Botkit on Glitch.com better
   require(__dirname + '/components/plugin_glitch.js')(controller);
 
-console.log("AAAAA")
 
 controller.hears( ['hello', 'hi', 'greetings'],
     ['direct_mention', 'mention', 'direct_message', 'ambient'],
@@ -132,7 +112,6 @@ controller.hears( ['hello', 'hi', 'greetings'],
     bot.reply(message, 'Hello!');
 });
  
-}
 
 
 
