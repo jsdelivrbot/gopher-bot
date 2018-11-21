01f6a92d38e3e8a945e2e65c254f6128c040ccf5
cdrbbt <egor.bykadorov@gmail.com>
2018-11-21 15:35:30 +0200

    acrh change

--- bot.js
+++ bot.js
@@ -114,74 +114,11 @@ var webserver = require(__dirname + '/components/express_webserver.js')(controll
   // Load in some helpers that make running Botkit on Glitch.com better
   require(__dirname + '/components/plugin_glitch.js')(controller);
 
-
-
-controller.hears( ['hello', 'hi', 'greetings'],
-    ['direct_mention', 'mention', 'direct_message', 'ambient'],
-     function (bot, message) {
-         let data = {}
-         console.log(message)
-         bot.api.channels.info({channel: message.channel}, function(error, response){
-             console.log(response.channel)
-             data.channel = response.channel.name
-
-             bot.api.users.info({user: message.user}, function(error, response){
-                data.user = response.user.real_name
-                console.log(response)
-
-                bot.reply(message, `Text: ${message.text}, channel ${data.channel},
-                 user ${data.user}`)
-                 var msg = new Message(
-                    {
-                        user: data.user,
-                        message: message.text,
-                        channel: data.channel,
-                        tags: ["Test", "not real"],
-                        time_send: "12 oclock"
-                    });
-                    
-                    msg.save(function (err) {
-                        if (err) {console.log(err)} 
-                        console.log("pass")
-                    });
-
-                 bot.reply(message, {
-                    attachments:[
-                        {
-                            title: 'Push to site',
-                            callback_id: '123',
-                            attachment_type: 'default',
-                            actions: [
-                                {
-                                    "name":"yes",
-                                    "text": "Yes, please",
-                                    "value": "da",
-                                    "type": "button",
-                                },
-                                {
-                                    "name":"no",
-                                    "text": "No dont",
-                                    "value": "nyet",
-                                    "type": "button",
-                                }
-                            ]
-                        }
-                    ]
-                });
-
-
-                 
-            })             
-         })
-
-        
-         
-
-    });
+  var normalizedPath = require("path").join(__dirname, "skills");
+  require("fs").readdirSync(normalizedPath).forEach(function(file) {
+    require("./skills/" + file)(controller);
+  });
  
-
-
-
 
 
 function usage_tip() {
