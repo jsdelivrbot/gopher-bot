8ebf43f6b7546ce8c17e9163bdb3021717c86582
cdrbbt <egor.bykadorov@gmail.com>
2018-11-26 11:40:39 +0200

    log everything

--- skills/reactions.js
+++ skills/reactions.js
@@ -2,10 +2,16 @@ module.exports = function (controller) {
     controller.on('reaction_added', function(bot, message){
         console.log('reaction added?')
         console.log(message)
+
+        console.log(message.event.item.channel)
+        console.log(message.event.item.timestamp)
+
         bot.api.reactions.get({
             channel: message.event.item.channel,
             timestamp: message.event.item.timestamp
         }, function(error, response){
+            console.log('api req here')
+            console.log(error)
             console.log(response)
         }
         )
