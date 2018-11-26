745dfb1b67e657d867e2f3451ae82a996aa7ae21
cdrbbt <egor.bykadorov@gmail.com>
2018-11-26 14:19:52 +0200

    controller isntead of bot

--- skills/test.js
+++ skills/test.js
@@ -2,6 +2,6 @@ var Message = require('../models/messageModel');
 
 module.exports = function (controller) {
     controller.on('ambient', function(bot, message){
-        bot.trigger(message.channel, [bot, message])
+        controller.trigger(message.channel, [bot, message])
     })
 }
