47d8c1689e912426345b7783659c0b863150e852
pessi22 <juhopessi1@gmail.com>
2018-11-26 12:43:33 +0200

    split string

--- skills/joinOnChannel.js
+++ skills/joinOnChannel.js
@@ -7,30 +7,34 @@ module.exports = function (controller) {
         function (bot, message) {
             let data = {}
             console.log(message)
-            bot.api.channels.info({channel: message.channel}, function (error, response) {
-                
+            bot.api.channels.info({ channel: message.channel }, function (error, response) {
+
                 splitString()
-                
+
                 function splitString() {
 
-                var tags = response.channel.purpose.value.split(" ")
-               
-                console.log(response.channel.purpose.value)
-                console.log('WTF' + tags)
-                
-                var channelInfo = new ChannelInfo(
-                    {
-                        channelId: response.channel.id,
-                        chanconsolenelName: response.channel.name,
-                        tags: tags
-                    });
+                    var tags = response.channel.purpose.value
+
+                    tags = tags.substring(str.indexOf("tags:") + 1)
+
+                    tags = response.channel.purpose.value.split(" ")
+
+                    console.log(response.channel.purpose.value)
+                    console.log('WTF' + tags)
+
+                    var channelInfo = new ChannelInfo(
+                        {
+                            channelId: response.channel.id,
+                            channelName: response.channel.name,
+                            tags: tags
+                        });
                     channelInfo.save(function (err) {
-                        if (err) {console.log(err)} 
+                        if (err) { console.log(err) }
                         console.log("channel info saved")
                     });
                 }
 
-               
+
             })
         });
 }
