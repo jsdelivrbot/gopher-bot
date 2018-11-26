22028efdf168816c8a1a190c0afff217fa39eee0
cdrbbt <egor.bykadorov@gmail.com>
2018-11-26 11:20:34 +0200

    fetch reactions

--- skills/reactions.js
+++ skills/reactions.js
@@ -2,5 +2,12 @@ module.exports = function (controller) {
     controller.on('reaction_added', function(bot, message){
         console.log('reaction added?')
         console.log(message)
+        bot.api.reactions.get({
+            channel: message.event.item.channel,
+            timestamp: message.event.item.timestamp
+        }, function(error, response){
+            log(response)
+        }
+        )
     })
 }
