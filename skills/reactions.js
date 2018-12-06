20ea5cd2a5aaddea5f3eb14a0d1ae3bf02eb90f7
cdrbbt <egor.bykadorov@gmail.com>
2018-12-06 15:18:26 +0200

    fetch channel name

--- skills/reactions.js
+++ skills/reactions.js
@@ -1,4 +1,5 @@
 var Message = require('../models/messageModel');
+var ChannelInfo = require('../models/channelInfoModel');
 
 module.exports = function (controller) {
     controller.on('reaction_added', function (bot, message) {
@@ -22,23 +23,27 @@ module.exports = function (controller) {
                 bot.api.users.info({ user: response.message.user }, function (error, response2) {
                     console.log('second response')
                     console.log(response2);
-                    let cont = {
-                        user: message.item_user,
-                        channel: message.team,
-                        tags: ["Reactive"],
-                        time_send: message.item.ts,
-                        message: response.message.text
-                    }
-                    dbmsg = new Message(cont)
-                    console.log(dbmsg);
-                    // dbmsg.save(function (err) {
-                    //     if (err) { console.log(err) }
-                    //     else {
-                    //         console.log("message saved")
-                    //     }
-                    // })
-
-
+                    ChannelInfo.findOne({channelId: message.item.channel}, (err, res)=>{
+                        
+                        let cont = {
+                            user: response2.user.real_name,
+                            channel: res.channelName,
+                            tags: ["Reactive"],
+                            time_send: Math.trunc(message.item.ts).toString,
+                            message: response.message.text
+                        }
+                        dbmsg = new Message(cont)
+                        console.log(dbmsg);
+                        // dbmsg.save(function (err) {
+                        //     if (err) { console.log(err) }
+                        //     else {
+                        //         console.log("message saved")
+                        //     }
+                        // })
+    
+    
+                    })
+                    
                 })
             }
         })
